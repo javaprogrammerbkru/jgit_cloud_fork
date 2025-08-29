8dc3fe4a9aa20205c8553302574cdb72cfb2a585
Sergey Kozlov <sergey.kozlov@gitkraken.com>
2025-08-29 15:51:41 +0500

    Test

---
+++ org.eclipse.jgit/src/com/bigbrassband/common/apache/HttpConstants.java
@@ -0,0 +1,124 @@
+package com.bigbrassband.common.apache;
+
+/**
+ * HttpConstants.
+ */
+public final class HttpConstants {
+
+    /**
+     * Cookie
+     */
+    public static final String COOKIE = "Cookie";
+    /**
+     * Set-Cookie
+     */
+    public static final String SET_COOKIE = "Set-Cookie";
+    /**
+     * x-amz-sns-message-id
+     */
+    public static final String X_AMZ_SNS_MESSAGE_ID = "x-amz-sns-message-id";
+
+    /**
+     * user-agent
+     */
+    public static final String USER_AGENT_FIELD = "user-agent";
+    /**
+     * x-bbb-webhook-type
+     */
+    public static final String X_BBB_WEBHOOK_TYPE_FIELD = "x-bbb-webhook-type";
+    /**
+     * x-github-event
+     */
+    public static final String X_GITHUB_EVENT_FIELD = "x-github-event";
+    /**
+     * x-gitlab-event
+     */
+    public static final String X_GITLAB_EVENT_FIELD = "x-gitlab-event";
+    /**
+     * x-origin-url
+     */
+    public static final String X_ORIGIN_URL_FIELD = "x-origin-url";
+
+    /**
+     * JWT
+     */
+    public static final String JWT_PREFIX = "JWT ";
+    /**
+     * application/vnd.github.v3+json
+     */
+    public static final String APPLICATION_VND_GITHUB_V3_JSON = "application/vnd.github.v3+json";
+    /**
+     * application/vnd.github+json
+     */
+    public static final String APPLICATION_VND_GITHUB_JSON = "application/vnd.github+json";
+    /**
+     * X-Forwarded-For
+     */
+    public static final String X_FORWARDED_FOR = "X-Forwarded-For";
+    /**
+     * X-RateLimit-Reset
+     */
+    public static final String X_RATELIMIT_RESET = "X-RateLimit-Reset";
+    /**
+     * x-ms-continuationtoken
+     */
+    public static final String X_MS_CONTINUATIONTOKEN = "x-ms-continuationtoken";
+    /**
+     * content-type
+     */
+    public static final String CONTENT_TYPE = "content-type";
+    /**
+     * x-bbb-webhook-type
+     */
+    public static final String X_BBB_WEBHOOK_TYPE = "x-bbb-webhook-type";
+    /**
+     * x-bbb-webhook-id
+     */
+    public static final String X_BBB_WEBHOOK_ID = "x-bbb-webhook-id";
+    /**
+     * x-amz-sns-message-type
+     */
+    public static final String X_AMZ_SNS_MESSAGE_TYPE = "x-amz-sns-message-type";
+    /**
+     * x-gitlab-event-uuid
+     */
+    public static final String X_GITLAB_EVENT_UUID = "x-gitlab-event-uuid";
+    /**
+     * x-request-uuid
+     */
+    public static final String X_REQUEST_UUID = "x-request-uuid";
+    /**
+     * x-event-key
+     */
+    public static final String X_EVENT_KEY = "x-event-key";
+    /**
+     * x-github-delivery
+     */
+    public static final String X_GITHUB_DELIVERY = "x-github-delivery";
+    /**
+     * x-vss-activityid
+     */
+    public static final String X_VSS_ACTIVITYID = "x-vss-activityid";
+
+    /**
+     * Bearer
+     */
+    public static final String BEARER_UPPER_SPACE = "Bearer ";
+    /**
+     * bearer
+     */
+    public static final String BEARER_LOWER_SPACE = "bearer ";
+    /**
+     * Bearer
+     */
+    public static final String BEARER_UPPER = "Bearer";
+
+    /**
+     * application/json
+     */
+    public static final String APPLICATION_JSON = "application/json";
+
+
+    private HttpConstants() {
+    }
+}
